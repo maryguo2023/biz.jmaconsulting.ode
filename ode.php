89ea1fa3269eb5504e3a8a3d1a7cfde13636cf4e
Pradeep Nayak <pradpnayak@gmail.com>
2017-12-18 18:22:44 +0530

    fixed indentation

--- ode.php
+++ ode.php
@@ -1,4 +1,4 @@
-<?php
+print (<?php
 
 require_once 'ode.civix.php';
 
@@ -284,92 +284,92 @@ function pluckEmailFromHeader($header) {
 }
 
 function get_domain($domain, $debug = FALSE) {
-	$original = $domain = strtolower($domain);
+  $original = $domain = strtolower($domain);
 
-	if (filter_var($domain, FILTER_VALIDATE_IP)) {
+  if (filter_var($domain, FILTER_VALIDATE_IP)) {
     return $domain;
   }
 
-	$debug ? print('<strong style="color:green">&raquo;</strong> Parsing: '.$original) : FALSE;
+  $debug ? print ('<strong style="color:green">&raquo;</strong> Parsing: '.$original) : FALSE;
 
-	$arr = array_slice(array_filter(explode('.', $domain, 4), function($value) {
-		return $value !== 'www';
-	}), 0); //rebuild array indexes
+  $arr = array_slice(array_filter(explode('.', $domain, 4), function($value) {
+    return $value !== 'www';
+  }), 0); //rebuild array indexes
 
-	if (count($arr) > 2) {
-		$count = count($arr);
-		$_sub = explode('.', $count === 4 ? $arr[3] : $arr[2]);
+  if (count($arr) > 2) {
+    $count = count($arr);
+    $_sub = explode('.', $count === 4 ? $arr[3] : $arr[2]);
 
-		$debug ? print(" (parts count: {$count})") : FALSE;
+    $debug ? print (" (parts count: {$count})") : FALSE;
 
-		if (count($_sub) === 2) {// two level TLD
-			$removed = array_shift($arr);
-			if ($count === 4) {// got a subdomain acting as a domain
-				$removed = array_shift($arr);
-			}
-			$debug ? print("<br>\n" . '[*] Two level TLD: <strong>' . join('.', $_sub) . '</strong> ') : FALSE;
-		}
-		elseif (count($_sub) === 1) {// one level TLD
-			$removed = array_shift($arr); //remove the subdomain
+    if (count($_sub) === 2) {// two level TLD
+      $removed = array_shift($arr);
+      if ($count === 4) {// got a subdomain acting as a domain
+        $removed = array_shift($arr);
+      }
+      $debug ? print ("<br>\n" . '[*] Two level TLD: <strong>' . implode('.', $_sub) . '</strong> ') : FALSE;
+    }
+    elseif (count($_sub) === 1) {// one level TLD
+      $removed = array_shift($arr); //remove the subdomain
 
-			if (strlen($_sub[0]) === 2 && $count === 3) {// TLD domain must be 2 letters
-				array_unshift($arr, $removed);
-			}
-			else{
-				// non country TLD according to IANA
-				$tlds = array(
-					'aero',
-					'arpa',
-					'asia',
-					'biz',
-					'cat',
-					'com',
-					'coop',
-					'edu',
-					'gov',
-					'info',
-					'jobs',
-					'mil',
-					'mobi',
-					'museum',
-					'name',
-					'net',
-					'org',
-					'post',
-					'pro',
-					'tel',
-					'travel',
-					'xxx',
-				);
+      if (strlen($_sub[0]) === 2 && $count === 3) {// TLD domain must be 2 letters
+        array_unshift($arr, $removed);
+      }
+      else{
+        // non country TLD according to IANA
+        $tlds = array(
+          'aero',
+          'arpa',
+          'asia',
+          'biz',
+          'cat',
+          'com',
+          'coop',
+          'edu',
+          'gov',
+          'info',
+          'jobs',
+          'mil',
+          'mobi',
+          'museum',
+          'name',
+          'net',
+          'org',
+          'post',
+          'pro',
+          'tel',
+          'travel',
+          'xxx',
+        );
 
-				if (count($arr) > 2 && in_array($_sub[0], $tlds) !== FALSE) {//special TLD don't have a country
-					array_shift($arr);
-				}
-			}
-			$debug ? print("<br>\n" . '[*] One level TLD: <strong>' . join('.', $_sub) . '</strong> ') : FALSE;
-		}
-		else {// more than 3 levels, something is wrong
-			for ($i = count($_sub); $i > 1; $i--) {
-				$removed = array_shift($arr);
-			}
-			$debug ? print("<br>\n" . '[*] Three level TLD: <strong>' . join('.', $_sub) . '</strong> ') : FALSE;
-		}
-	}
-	elseif (count($arr) === 2) {
-		$arr0 = array_shift($arr);
+        if (count($arr) > 2 && in_array($_sub[0], $tlds) !== FALSE) {//special TLD don't have a country
+          array_shift($arr);
+        }
+      }
+      $debug ? print ("<br>\n" . '[*] One level TLD: <strong>' . implode('.', $_sub) . '</strong> ') : FALSE;
+    }
+    else {// more than 3 levels, something is wrong
+      for ($i = count($_sub); $i > 1; $i--) {
+        $removed = array_shift($arr);
+      }
+      $debug ? print ("<br>\n" . '[*] Three level TLD: <strong>' . implode('.', $_sub) . '</strong> ') : FALSE;
+    }
+  }
+  elseif (count($arr) === 2) {
+    $arr0 = array_shift($arr);
 
-		if (strpos(join('.', $arr), '.') === FALSE
-			&& in_array($arr[0], array('localhost','test','invalid')) === FALSE
+    if (strpos(implode('.', $arr), '.') === FALSE
+      && in_array($arr[0], array('localhost','test','invalid')) === FALSE
     ) { // not a reserved domain
-			$debug ? print("<br>\n" .'Seems invalid domain: <strong>'.join('.', $arr).'</strong> re-adding: <strong>'.$arr0.'</strong> ') : FALSE;
-			// seems invalid domain, restore it
-			array_unshift($arr, $arr0);
-		}
-	}
+      $debug ? print ("<br>\n" . 'Seems invalid domain: <strong>' . implode('.', $arr) . '</strong> re-adding: <strong>' . $arr0 . '</strong> ') : FALSE;
+      // seems invalid domain, restore it
+      array_unshift($arr, $arr0);
+    }
+  }
 
-	$debug ? print("<br>\n" . '<strong style="color:gray">&laquo;</strong> Done parsing: <span style="color:red">' . $original . '</span> as <span style="color:blue">' . join('.', $arr) . "</span><br>\n") : FALSE;
+  $debug ? print ("<br>\n" . '<strong style="color:gray">&laquo;</strong> Done parsing: <span style="color:red">' . $original . '</span> as <span style="color:blue">' . implode('.', $arr) . "</span><br>\n") : FALSE;
 
-	return join('.', $arr);
+  return implode('.', $arr);
 }
 
 
